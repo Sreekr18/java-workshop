215e20b143284921ecadf2946c2cd5a32f94801c
sreekr18 <sreekr18@gmail.com>
2025-07-02 14:51:18 +0530

    sum of my marks

---
+++ TotalMarks.java
@@ -0,0 +1,12 @@
+public class TotalMarks {
+    public static void main(String[] args) {
+        int[] marks = {85, 90, 78, 92, 88};
+        int total = 0;
+
+        for (int mark : marks) {
+            total += mark;
+        }
+
+        System.out.println("Total Marks: " + total);
+    }
+}
