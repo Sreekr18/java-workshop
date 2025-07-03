55b18ebd1c47b7f7eb5250cc1eba88bd30a8f8f4
Sreekr18 <sreekr18@gmail.com>
2025-07-03 07:04:03 +0530

    Create HighestBreadPrice.java

---
+++ src/com/vetias/java/workshop/basics/HighestBreadPrice.java
@@ -0,0 +1,14 @@
+public class HighestBreadPrice {
+
+    public static void main(String[] args) {
+        int[] breadPrices = {85, 90, 95, 100, 105};
+        int highestPrice = breadPrices[0];
+
+        for (int i = 1; i < breadPrices.length; i++) {
+            if (breadPrices[i] > highestPrice) {
+                highestPrice = breadPrices[i];
+            }
+        }
+        System.out.println("The highest price of bread is: " + highestPrice);
+    }
+}
