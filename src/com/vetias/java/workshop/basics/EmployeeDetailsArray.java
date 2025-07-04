794b6f58beda3ab029c6ea1258c5368325d7c657
sreekr18 <sreekr18@gmail.com>
2025-07-04 10:20:32 +0530

    updated

--- src/com/vetias/java/workshop/basics/EmployeeDetailsArray.java
+++ src/com/vetias/java/workshop/basics/EmployeeDetailsArray.java
@@ -1,19 +1,19 @@
 public class EmployeeDetailsArray {
     public static void main(String[] args) {
         String[][] employees=new String[5][2];
-        employees[0][0] = "101";
+        employees[0][0] = "Rs.50000";
         employees[0][1] = "Anand";
-        employees[1][0] = "102";
+        employees[1][0] = "Rs.60000";
         employees[1][1] = "Bharat";
-        employees[2][0] = "103";
+        employees[2][0] = "Rs.70000";
         employees[2][1] = "Ram";
-        employees[3][0] = "104";
+        employees[3][0] = "Rs.55000";
         employees[3][1] = "Dinesh";
-        employees[4][0] = "105";
+        employees[4][0] = "Rs.83000";
         employees[4][1] = "Suresh";
         System.out.println("Employee Details:");
         for (int i = 0; i < employees.length; i++) {
-            System.out.println("Roll No: " + employees[i][0] + ", Name: " + employees[i][1]);
+            System.out.println("Salary: " + employees[i][0] + ", Name: " + employees[i][1]);
         }
 
     }
