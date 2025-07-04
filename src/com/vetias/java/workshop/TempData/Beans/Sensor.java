f1d4ee17c04bf97b2b18f5573a97080b840054be
sreekr18 <sreekr18@gmail.com>
2025-07-04 11:54:47 +0530

    UPDATED

--- src/com/vetias/java/workshop/TempData/Beans/Sensor.java
+++ src/com/vetias/java/workshop/TempData/Beans/Sensor.java
@@ -1,36 +1,4 @@
-public class Sensor {
-    private String sensorID;
-    private String name;
-    private double temperature;
-
-    public Sensor(String sensorID, String name, double temperature) {
-        this.sensorID = sensorID;
-        this.name = name;
-        this.temperature = temperature;
-    }
-
-    public String getSensorID() {
-        return sensorID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getTemperature() {
-        return temperature;
-    }
-
-    public void setSensorID(String sensorID) {
-        this.sensorID = sensorID;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setTemperature(double temperature) {
-        this.temperature = temperature;
-    }
-
+public interface Sensor{
+    public float getReading();
+    
 }
