64437ce5094016b60e5a0e7f71b061b504e1d93c
sreekr18 <sreekr18@gmail.com>
2025-07-07 12:21:55 +0530

    updated

--- src/com/vetias/java/workshop/basics/Words.java
+++ src/com/vetias/java/workshop/basics/Words.java
@@ -6,7 +6,9 @@ public class Words {
         String[] chr = text.split("");
         System.out.println("Number of sentences: " + sentences.length);
         System.out.println("Number of words: " + words.length);
+        //since "text" is a string, we can use the length() method to get the number of characters but not "text.length"
         System.out.println("Number of characters:"+text.length());
+        //here we used "chr.length" to get the number of characters in the string "text" after splitting it into individual arrays.Because of making it array. we used the "chr.length" to get the number of characters.
         System.out.println("Number of characters: " + chr.length); 
         
        
