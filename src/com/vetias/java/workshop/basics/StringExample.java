fa1cc0e4783e7ee9cb4f038d078019970bd11cd3
sreekr18 <sreekr18@gmail.com>
2025-07-07 10:27:30 +0530

    updated

--- src/com/vetias/java/workshop/basics/StringExample.java
+++ src/com/vetias/java/workshop/basics/StringExample.java
@@ -2,7 +2,8 @@ public class StringExample {
     public static void main(String[] args) {
         String FirstName = "Sree";
         String LastName = "Krishnan";
-        String FinalName = FirstName.concat(LastName);
+        String ExtraName = " R";
+        String FinalName = FirstName.concat(LastName).concat(ExtraName);
         System.out.println("Concatenated String: " + FinalName);
         String UpperCase = FinalName.toUpperCase();
         System.out.println("Uppercase String: " +UpperCase);
