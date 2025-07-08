21c1ffd53322abcfdd5fc6acaec6b6ac4190aaf1
sreekr18 <sreekr18@gmail.com>
2025-07-08 15:54:58 +0530

    updated

--- src/com/vetias/java/workshop/basics/ArithCalculator.java
+++ src/com/vetias/java/workshop/basics/ArithCalculator.java
@@ -1,22 +1,28 @@
-import java.util.Scanner;
+
 
 public class ArithCalculator {
-    public static void main(String[] args) {
-        int a=3,b=0;
-        System.out.println(a+b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        try{
+    int a=3,b=0;{
+
+    
+     try{
             if (b==0) 
             {
                 throw new ArithmeticException("0 cant be a divisor");
-
-                
             }
-        }catch(ArithmeticException e){
+        }
+        catch(ArithmeticException e)
+        {
                 System.out.println(e.getMessage());
         }
-            
     }
 
+    public static void main(String[] args) {
+        System.out.println(ArithCalculator.add(a+b));
+        System.out.println(a*b);
+        System.out.println(a/b);
+        
+       
+        ArithCalculator calculator=new ArithCalculator();
+
+    }
 }
