462569526c1a0247630d2b5a819fe2819c86e0c0
sreekr18 <sreekr18@gmail.com>
2025-07-10 14:21:55 +0530

    updated

---
+++ src/main/java/com/vetias/java/workshop/basics/ListExample.java
@@ -0,0 +1,27 @@
+package com.vetias.java.workshop.basics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+    public static void main(String[] args) {
+       List<String> names = new ArrayList<>();
+       names.add("sree");
+       names.add("krish");
+       names.add("anada");
+       names.add("dharan");
+       names.add("tr");
+       names.add("ranjith");
+       System.out.println(names);
+       for(String name:names){
+        System.out.println(name);   
+       }
+       System.out.println("another method:");
+       for(int i=0;i<names.size();i++){
+        System.out.println(names.get(i));   
+       }
+
+    
+}
+}
+
