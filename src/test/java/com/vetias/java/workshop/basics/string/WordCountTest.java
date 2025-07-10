792e8c9f5727edc3537ff4f65c6b44bbea1753ce
sreekr18 <sreekr18@gmail.com>
2025-07-10 12:21:16 +0530

    updated

--- src/test/java/com/vetias/java/workshop/basics/string/WordCountTest.java
+++ src/test/java/com/vetias/java/workshop/basics/string/WordCountTest.java
@@ -1,14 +1,20 @@
 package com.vetias.java.workshop.basics.string;
 import org.junit.jupiter.api.Assertions;
-import com.vetias.java.workshop.basics.string.WordCount;
 import org.junit.jupiter.api.Test;
+import com.vetias.java.workshop.basics.string.WordCount;
 
 public class WordCountTest {
     @Test
     public void testWordcount(){
         WordCount wordCount=new WordCount();
         int count = wordCount.count("this program shows");
-        Assertions.assertEquals(12, count);
+        Assertions.assertEquals(3, count);
+    }
+    @Test
+    public void testAnotherString(){
+        WordCount wordCount=new WordCount();
+        int count = wordCount.count("this program shows how the things are done");
+        Assertions.assertEquals(8, count);
     }
 
 }
