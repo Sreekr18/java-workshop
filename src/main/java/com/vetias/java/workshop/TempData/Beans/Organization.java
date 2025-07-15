2cc248e5b761f5c13e4873ea4739244b5fb1ab13
sreekr18 <sreekr18@gmail.com>
2025-07-15 10:24:01 +0530

    updated

--- src/main/java/com/vetias/java/workshop/TempData/Beans/Organization.java
+++ src/main/java/com/vetias/java/workshop/TempData/Beans/Organization.java
@@ -3,12 +3,11 @@ package com.vetias.java.workshop.TempData.Beans;
 import java.time.LocalDate;
 
 public record Organization(String name,
-String completeAddress,
+String description,
 String website,
 String email,
 String contactNumber,
 long registrationNumber,
-String description,
 LocalDate registrationDate) {
 
     
--- src/main/java/com/vetias/java/workshop/TempData/dao/JDBCExample.java
+++ src/main/java/com/vetias/java/workshop/TempData/dao/JDBCExample.java
@@ -1,9 +1,33 @@
 package com.vetias.java.workshop.TempData.dao;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import com.vetias.java.workshop.TempData.dao.OrganizationDAO;
 public class JDBCExample {
     public static void main(String[] args) {
-        OrganizationDAO organizationDAO = new OrganizationDAO();
-        
-        System.out.println("Organization table created successfully.");
-    }
+        Connection dbConnection = null;
+        try
+        {
+            Class.forName("org.h2.Driver");
+        }
+        catch(ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }    
+        try 
+        {
+            dbConnection = 
+            DriverManager.getConnection(
+                "jdbc:h2:mem:tempdataDB", "sa", null);
+        } 
+        catch(SQLException sqlException)
+        {
+            System.out.println("Error Creating table: " + sqlException);
+        }                                        
+    
 }
+}
+
+    
